673ed3d156c3a703bf0ea110779f588995f2b65d
Trí <tranhuuminhtri0310@gmail.com>
2026-02-06 16:15:58 +0700

    init project

---
+++ src/main/java/com/myproject/nabe_bank/NabeBankApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.nabe_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabeBankApplication.class, args);
+	}
+
+}
